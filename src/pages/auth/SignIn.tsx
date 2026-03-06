09470faece6bbd43825125b98e6eecd448eb3b16
Ayush <Win@Len>
2026-03-06 18:04:15 +0530

    feat: force google account selection screen

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -122,7 +122,10 @@ export default function SignIn() {
                                 const { error } = await supabase.auth.signInWithOAuth({
                                     provider: 'google',
                                     options: {
-                                        redirectTo: `${window.location.origin}`
+                                        redirectTo: `${window.location.origin}`,
+                                        queryParams: {
+                                            prompt: 'select_account'
+                                        }
                                     }
                                 })
                                 if (error) throw error
