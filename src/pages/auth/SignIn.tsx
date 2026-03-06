7afaeff2a93073c3377fb4992c3ddf488443c3b0
Ayush <Win@Len>
2026-03-06 18:01:13 +0530

    fix: improve google auth redirect logic for deployment

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -122,7 +122,7 @@ export default function SignIn() {
                                 const { error } = await supabase.auth.signInWithOAuth({
                                     provider: 'google',
                                     options: {
-                                        redirectTo: window.location.origin
+                                        redirectTo: `${window.location.origin}`
                                     }
                                 })
                                 if (error) throw error
